e0125c0627373a5bdf41567aa889b3b29e588b52
Ben Kasper <53241710+bckasper3@users.noreply.github.com>
2024-11-24 17:38:03 -0500

    Finished the calculators - time to start charts

--- js/PPHcalc.js
+++ js/PPHcalc.js
@@ -21,49 +21,48 @@ function calculate1() {
 
 if (varPPHtoGPMcheck.checked) {
   //GPM TO PPH CHECK BOX
-  console.log('event listener function');
   switch (operation1) {
     case "JetA":
       result_SG = SG_JetA.toFixed(4);
       result_Density = Density_JetA.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);      //Rounds to 3 decimal places
-      result_gpm = (input2/(SG_JetA*0.0360912)).toFixed(5);    //Rounds to 5 decimal places
+      result_inches3sec = (input2*3.85).toFixed(4);      //Rounds to 3 decimal places
+      result_gpm = (input2*Density_JetA*60).toFixed(5);    //Rounds to 5 decimal places
       break;
     case "JetB":
       result_SG = SG_JetB.toFixed(4);
       result_Density = Density_JetB.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);       
-      result_gpm = (input2/(SG_JetB*0.0360912)).toFixed(5);
+      result_inches3sec = (input2*3.85).toFixed(4);       
+      result_gpm = (input2*Density_JetB*60).toFixed(5);
       break;
     case "JP4":
       result_SG = SG_JP_4.toFixed(4);
       result_Density = Density_JP_4.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);       
-      result_gpm = (input2/(SG_JP_4*0.0360912)).toFixed(5);
+      result_inches3sec = (input2*3.85).toFixed(4);       
+      result_gpm = (input2*Density_JP_4*60).toFixed(5);
       break;
     case "JP5":
       result_SG = SG_JP_5.toFixed(4);
       result_Density = Density_JP_5.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);       
-      result_gpm = (input2/(SG_JP_5*0.0360912)).toFixed(5);
+      result_inches3sec = (input2*3.85).toFixed(4);       
+      result_gpm = (input2*Density_JP_5*60).toFixed(5);
       break;
     case "JP8":
       result_SG = SG_JP_8.toFixed(4);
       result_Density = Density_JP_8.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);       
-      result_gpm = (input2/(SG_JP_8*0.0360912)).toFixed(5);
+      result_inches3sec = (input2*3.85).toFixed(4);       
+      result_gpm = (input2*Density_JP_8*60).toFixed(5);
       break;
     case "AvGas":
       result_SG = SG_AvGas.toFixed(4);
       result_Density = Density_AvGas.toFixed(4);
-      result_inches3sec = (input2/0.0360912).toFixed(4);       
-      result_gpm = (input2/(SG_AvGas*0.0360912)).toFixed(5);
+      result_inches3sec = (input2*3.85).toFixed(4);       
+      result_gpm = (input2*Density_AvGas*60).toFixed(5);
       break;
     case "StoddardSolvent":
         result_SG = SG_StoddardSolvent.toFixed(4);
         result_Density = Density_StoddardSolvent.toFixed(4);
-        result_inches3sec = (input2/0.0360912).toFixed(4);       
-        result_gpm = (input2/(SG_StoddardSolvent*0.0360912)).toFixed(5);
+        result_inches3sec = (input2*3.85).toFixed(4);       
+        result_gpm = (input2*Density_StoddardSolvent*60).toFixed(5);
         break;      
     default:
       result_SG = "";
@@ -72,49 +71,49 @@ if (varPPHtoGPMcheck.checked) {
       result_gpm = "";
     }
 } else {
-  //INCHES OF FUEL CHECKBOX
+  //PPH TO GPM CHECKBOX
     switch (operation1) {
       case "JetA":
         result_SG = SG_JetA.toFixed(4);
         result_Density = Density_JetA.toFixed(4);
-        result_inches3sec = (input2*SG_JetA).toFixed(4);           //Rounds to 3 decimal places
-        result_gpm = ((input2*SG_JetA)*0.0360912).toFixed(5);    //Rounds to 5 decimal places
+        result_inches3sec = ((input2*231)/(3600*Density_JetA)).toFixed(4);           //Rounds to 3 decimal places
+        result_gpm = (input2/(Density_JetA*60)).toFixed(5);    //Rounds to 5 decimal places
         break;
       case "JetB":
         result_SG = SG_JetB.toFixed(4);
         result_Density = Density_JetB.toFixed(4);
-        result_inches3sec = (input2*SG_JetB).toFixed(4);       
-        result_gpm = ((input2*SG_JetB)*0.0360912).toFixed(5);
+        result_inches3sec = ((input2*231)/(3600*Density_JetB)).toFixed(4);       
+        result_gpm = (input2/(Density_JetB*60)).toFixed(5);
         break;
       case "JP4":
         result_SG = SG_JP_4.toFixed(4);
         result_Density = Density_JP_4.toFixed(4);
-        result_inches3sec = (input2*SG_JP_4).toFixed(4);       
-        result_gpm = ((input2*SG_JP_4)*0.0360912).toFixed(5);
+        result_inches3sec = ((input2*231)/(3600*Density_JP_4)).toFixed(4);       
+        result_gpm = (input2/(Density_JP_4*60)).toFixed(5);
         break;
       case "JP5":
         result_SG = SG_JP_5.toFixed(4);
         result_Density = Density_JP_5.toFixed(4);
-        result_inches3sec = (input2*SG_JP_5).toFixed(4);       
-        result_gpm = ((input2*SG_JP_5)*0.0360912).toFixed(5);
+        result_inches3sec = ((input2*231)/(3600*Density_JP_5)).toFixed(4);       
+        result_gpm = (input2/(Density_JP_5*60)).toFixed(5);
         break;
       case "JP8":
         result_SG = SG_JP_8.toFixed(4);
         result_Density = Density_JP_8.toFixed(4);
-        result_inches3sec = (input2*SG_JP_8).toFixed(4);       
-        result_gpm = ((input2*SG_JP_8)*0.0360912).toFixed(5);
+        result_inches3sec = ((input2*231)/(3600*Density_JP_8)).toFixed(4);       
+        result_gpm = (input2/(Density_JP_8*60)).toFixed(5);
         break;
       case "AvGas":
         result_SG = SG_AvGas.toFixed(4);
         result_Density = Density_AvGas.toFixed(4);
-        result_inches3sec = (input2*SG_AvGas).toFixed(4);       
-        result_gpm = ((input2*SG_AvGas)*0.0360912).toFixed(5);
+        result_inches3sec = ((input2*231)/(3600*Density_AvGas)).toFixed(4);       
+        result_gpm = (input2/(Density_AvGas*60)).toFixed(5);
         break;
       case "StoddardSolvent":
           result_SG = SG_StoddardSolvent.toFixed(4);
           result_Density = Density_StoddardSolvent.toFixed(4);
-          result_inches3sec = (input2*SG_StoddardSolvent).toFixed(4);       
-          result_gpm = ((input2*SG_StoddardSolvent)*0.0360912).toFixed(5);
+          result_inches3sec = ((input2*231)/(3600*Density_StoddardSolvent)).toFixed(4);       
+          result_gpm = (input2/(Density_StoddardSolvent*60)).toFixed(5);
           break;      
       default:
         result_SG = "";
@@ -144,7 +143,6 @@ const varpphgpmCalcResult2 = document.getElementById('pphgpmCalcResult2');
 
 // Add an event listener to the checkbox to monitor changes
 varPPHtoGPMcheck.addEventListener('change', function() { // need to try: click, change, and input
-  console.log('event listener in the slider');
   if (varPPHtoGPMcheck.checked) {
     varpphgpmCalcOption1.style.display = 'none'; // Show element when checked
     varpphgpmCalcOption2.style.display = 'inline'; // Show element when unchecked
