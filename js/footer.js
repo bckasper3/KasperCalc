3854c5fac64fd8d685c009dfad3d0bdebc40b1f2
Ben Kasper <53241710+bckasper3@users.noreply.github.com>
2026-05-26 07:23:12 -0400

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -20,26 +20,95 @@
 
         <div class="footerRow">
           <h4 class="FooterRowTitle"><span class="footerflexbox">Fluids Calculators</span></h4>
-          <div class="footerlink"><a href="fuelHead.html">Jet Fuel Head Pressure</a></div>
-          <div class="footerlink"><a href="PPH.html">Jet Fuel PPH to GPM</a></div>
-          <div class="footerlink"><a href="universalfuelHead.html">Universal Fluid Head</a></div>
-          <div class="footerlink"><a href="universalPPH.html">Universal PPH to GPM</a></div>
-          <div class="footerlink"><a href="CrazyUniversalfuelHead.html">Water Density Compensated Universal Fluid Head</a></div>
-          <div class="footerlink"><a href="CrazyUniversalPPH.html">Water Density Compensated Universal PPH to GPM</a></div>
+          <div class="footerlink"><a href="fuelHead.html">Jet Fuel Head Calculator</a></div>
+          <div class="footerlink"><a href="PPH.html">Jet Fuel PPH to GPM Calculator</a></div>
+          <div class="footerlink"><a href="universalfuelHead.html">Universal Fluid Head Calculator</a></div>
+          <div class="footerlink"><a href="universalPPH.html">Universal PPH to GPM Calculator</a></div>
+          <div class="footerlink"><a href="CrazyUniversalfuelHead.html">Water Density Compensated Universal Fluid Head Calculator</a></div>
+          <div class="footerlink"><a href="CrazyUniversalPPH.html">Water Density Compensated Universal PPH to GPM Calculator</a></div>
           <div class="footerlink"><a href="equivalentFlows.html">Density Dependent Flow Rate Calculator</a></div>
           <div class="footerlink"><a href="MassFlowConverter.html">Mass Flow Rate Calculator</a></div>
         </div>
 
         <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">Spring Calculators</span></h4>
-          <div class="footerlink"><a href="SpringCompressionRound.html">Compression Spring Calculator</a></div>
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Jet Fuel</span></h4>
+          <div class="footerlink"><a href="nominalDensity.html">Nominal Density</a></div>
+          <div class="footerlink"><a href="minmaxDensity.html">Min/Max Density</a></div>
+          <div class="footerlink"><a href="kinematicViscosity.html">Viscosity</a></div>
+          <div class="footerlink"><a href="surfaceTension.html">Surface Tension</a></div>
+          <div class="footerlink"><a href="vaporPressure.html">Vapor Pressure</a></div>
+          <div class="footerlink"><a href="MultipleOfVolume.html">Multiple of Volume</a></div>
+          <div class="footerlink"><a href="distillationCurves.html">Distillation Curves</a></div>
         </div>
 
         <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">MIL-HDBK-5</span></h4>
-          <div class="footerlink"><a href="Chapter1(general).html">Chapter 1 (General)</a></div>
-          <div class="footerlink"><a href="Chapter2(steel).html">Chapter 2 (Steel)</a></div>
-          <div class="footerlink"><a href="Chapter3(aluminum).html">Chapter 3 (Aluminum)</a></div>
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Propylene Glycol</span></h4>
+          <div class="footerlink"><a href="PGPropertiesGeneral.html">General Properties</a></div>
+          <div class="footerlink"><a href="PGFrostProtection.html">Frost Protection</a></div>
+          <div class="footerlink"><a href="PGdensity.html">Density</a></div>
+          <div class="footerlink"><a href="PGheatcapacity.html">Specific Heat Capacity</a></div>
+          <div class="footerlink"><a href="PGthermalconductivity.html">Thermal Conductivity</a></div>
+          <div class="footerlink"><a href="PGviscosity.html">Viscosity</a></div>
+          <div class="footerlink"><a href="PGPrandltNumber.html">Prandtl Number</a></div>
+          <div class="footerlink"><a href="PGThermalExpansionCoefficient.html">Thermal Expansion Coefficient</a></div>
+          <div class="footerlink"><a href="PGBoilingPoint.html">Boiling Point</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Ethylene Glycol</span></h4>
+          <div class="footerlink"><a href="EGPropertiesGeneral.html">General Properties</a></div>
+          <div class="footerlink"><a href="EGFrostProtection.html">Frost Protection</a></div>
+          <div class="footerlink"><a href="EGdensity.html">Density</a></div>
+          <div class="footerlink"><a href="EGheatcapacity.html">Specific Heat Capacity</a></div>
+          <div class="footerlink"><a href="EGthermalconductivity.html">Thermal Conductivity</a></div>
+          <div class="footerlink"><a href="EGviscosity.html">Viscosity</a></div>
+          <div class="footerlink"><a href="EGPrandltNumber.html">Prandtl Number</a></div>
+          <div class="footerlink"><a href="EGThermalExpansionCoefficient.html">Thermal Expansion Coefficient</a></div>
+          <div class="footerlink"><a href="EGBoilingPoint.html">Boiling Point</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">MIL-PRF-23699</span></h4>
+          <div class="footerlink"><a href="MIL-PRF-23699PropertiesGeneral.html">General Properties</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699Density.html">Density</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699SpecificHeatCapacity.html">Specific Heat Capacity</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699BoilingHighPressure.html">Boiling Points at Pressures</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699thermalconductivity.html">Thermal Conductivity</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699viscosity.html">Dynamic and Kinematic Viscosity</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699PrandltNumber.html">Prandtl Number</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699EnthalpyEntropy.html">Enthalpy and Entropy</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699ThermalExpansionCoefficient.html">Thermal Expansion Coefficient</a></div>
+          <div class="footerlink"><a href="MIL-PRF-23699ThermalDiffusivity.html">Thermal Diffusivity</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Water</span></h4>
+          <div class="footerlink"><a href="WaterPropertiesGeneral.html">General Properties</a></div>
+          <div class="footerlink"><a href="densityWater.html">Density</a></div>
+          <div class="footerlink"><a href="WaterSpecificHeatCapacity.html">Specific Heat Capacity</a></div>
+          <div class="footerlink"><a href="WaterBoilingHighPressure.html">Boiling Points at Pressures</a></div>
+          <div class="footerlink"><a href="Waterthermalconductivity.html">Thermal Conductivity</a></div>
+          <div class="footerlink"><a href="Waterviscosity.html">Viscosity</a></div>
+          <div class="footerlink"><a href="WaterPrandltNumber.html">Prandtl Number</a></div>
+          <div class="footerlink"><a href="WaterEnthalpyEntropy.html">Enthalpy and Entropy</a></div>
+          <div class="footerlink"><a href="WaterIonizationConstant.html">Ionization Constant (pK<sub>w</sub>)</a></div>
+          <div class="footerlink"><a href="WaterHeatVaporization.html">Heat of Vaporization</a></div>
+          <div class="footerlink"><a href="WaterSaturationPressure.html">Saturation Pressure</a></div>
+          <div class="footerlink"><a href="WaterLiquidGasEquilibrium.html">Liquid Gas Equilibrium</a></div>
+          <div class="footerlink"><a href="WaterMeltingPointsHighPressure.html">Melting Points at High Pressure</a></div>
+          <div class="footerlink"><a href="WaterThermalExpansionCoefficient.html">Thermal Expansion Coefficient</a></div>
+          <div class="footerlink"><a href="WaterThermalDiffusivity.html">Thermal Diffusivity</a></div>
+          <div class="footerlink"><a href="WaterDissolvedGasDiffusionCoefficients.html">Gasses Dissolved in Water - Diffusion Coefficients</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Thermodyanmics Land</span></h4>
+          <div class="footerlink"><a href="ThermoDyanmicsLand.html">Thermodyanmics Land</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Spring Calculators</span></h4>
+          <div class="footerlink"><a href="SpringCompressionRound.html">Compression Spring Calculator</a></div>
         </div>
 
         <div class="footerRow">
@@ -53,50 +122,17 @@
         </div>
 
         <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">Jet Fuel</span></h4>
-          <div class="footerlink"><a href="minmaxDensity.html">Min/Max Density</a></div>
-          <div class="footerlink"><a href="nominalDensity.html">Nominal Density</a></div>
-          <div class="footerlink"><a href="kinematicViscosity.html">Kinematic Viscosity</a></div>
-          <div class="footerlink"><a href="surfaceTension.html">Surface Tension</a></div>
-          <div class="footerlink"><a href="vaporPressure.html">Vapor Pressure</a></div>
-          <div class="footerlink"><a href="MultipleOfVolume.html">Multiple of Volume</a></div>
-          <div class="footerlink"><a href="distillationCurves.html">Distillation Curves</a></div>
-        </div>
-
-        <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">Propylene Glycol</span></h4>
-          <div class="footerlink"><a href="PGPropertiesGeneral.html">Propylene Glycol General Properties</a></div>
-          <div class="footerlink"><a href="PGFrostProtection.html">Propylene Glycol Frost Protection</a></div>
-          <div class="footerlink"><a href="PGdensity.html">Propylene Glycol Density</a></div>
-          <div class="footerlink"><a href="PGheatcapacity.html">Propylene Glycol Specific Heat Capacity</a></div>
-          <div class="footerlink"><a href="PGthermalconductivity.html">Propylene Glycol Thermal Conductivity</a></div>
-          <div class="footerlink"><a href="PGviscosity.html">Propylene Glycol Viscosity</a></div>
-          <div class="footerlink"><a href="PGPrandltNumber.html">Propylene Glycol Prandtl Number</a></div>
-          <div class="footerlink"><a href="PGThermalExpansionCoefficient.html">Propylene Glycol Thermal Expansion Coefficient</a></div>
-          <div class="footerlink"><a href="PGBoilingPoint.html">Propylene Glycol Boiling Point</a></div>
-        </div>
-
-        <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">Ethylene Glycol</span></h4>
-          <div class="footerlink"><a href="EGPropertiesGeneral.html">Ethylene Glycol General Properties</a></div>
-          <div class="footerlink"><a href="EGFrostProtection.html">Ethylene Glycol Frost Protection</a></div>
-          <div class="footerlink"><a href="EGdensity.html">Ethylene Glycol Density</a></div>
-          <div class="footerlink"><a href="EGheatcapacity.html">Ethylene Glycol Specific Heat Capacity</a></div>
-          <div class="footerlink"><a href="EGthermalconductivity.html">Ethylene Glycol Thermal Conductivity</a></div>
-          <div class="footerlink"><a href="EGviscosity.html">Ethylene Glycol Viscosity</a></div>
-          <div class="footerlink"><a href="EGPrandltNumber.html">Ethylene Glycol Prandtl Number</a></div>
-          <div class="footerlink"><a href="EGThermalExpansionCoefficient.html">Ethylene Glycol Thermal Expansion Coefficient</a></div>
-          <div class="footerlink"><a href="EGBoilingPoint.html">Ethylene Glycol Boiling Point</a></div>
-        </div>
-
-        <div class="footerRow">
-          <h4 class="FooterRowTitle"><span class="footerflexbox">Misc Properties</span></h4>
-          <div class="footerlink"><a href="densityWater.html">Water Density</a></div>
-          <div class="footerlink"><a href="WaterSpecificHeatCapacity.html">Water Specific Heat Capacity</a></div>
-          <div class="footerlink"><a href="WaterDissolvedGasDiffusionCoefficients.html">Gasses Dissolved in Water - Diffusion Coefficients</a></div>
+          <h4 class="FooterRowTitle"><span class="footerflexbox">Misc</span></h4>
           <div class="footerlink"><a href="densityAirAltitude.html">Air Pressure from Altitude</a></div>
           <div class="footerlink"><a href="SpecificGravCalculator.html">Temperature Compensated Specific Gravity Calculator</a></div>
           <div class="footerlink"><a href="SpecificGravityExplanation.html">Specific Gravity Explanation</a></div>
+        </div>
+
+        <div class="footerRow">
+          <h4 class="FooterRowTitle"><span class="footerflexbox">MIL-HDBK-5</span></h4>
+          <div class="footerlink"><a href="Chapter1(general).html">Chapter 1 (General)</a></div>
+          <div class="footerlink"><a href="Chapter2(steel).html">Chapter 2 (Steel)</a></div>
+          <div class="footerlink"><a href="Chapter3(aluminum).html">Chapter 3 (Aluminum)</a></div>
         </div>
 
         <div class="footerRow">
